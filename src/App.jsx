8fc1a0510368a42cd1b8b993418e5a397fc511d0
Olena Deynega <suntrotter@gmail.com>
2024-12-07 08:15:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import data from "./contacts.json";
+import { nanoid } from "nanoid";
+import css from "./App.module.css";
+
+function App() {
+  const [contact, setContact] = useState(
+    () => JSON.parse(localStorage.getItem("contacts")) || data
+  );
+
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    if (contact.length === 0) {
+      localStorage.removeItem("contacts");
+    } else {
+      localStorage.setItem("contacts", JSON.stringify(contact));
+    }
+  }, [contact]);
+
+  const handleDelete = (id) => {
+    setContact(contact.filter((cont) => cont.id !== id));
+  };
+
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const searchedContacts = contact.filter((cont) =>
+    cont.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const handleSubmit = (values, actions) => {
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+    setContact([...contact, newContact]);
+    actions.resetForm();
+  };
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <SearchBox searchValue={searchValue} onSearch={handleSearch} />
+      <ContactList contacts={searchedContacts} onDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default App;
